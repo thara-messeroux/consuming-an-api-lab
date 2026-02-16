1bde3a8d354512b4e5d31f009c34eadf930112a2
thara-messeroux <tharamesseroux@gmail.com>
2026-02-15 23:28:17 -0500

    Initial project setup with Node, Express, EJS, and dotenv

--- server.js
+++ server.js
@@ -1,4 +1,1 @@
-a44ee8f272c348ededd4ae1a3318b1a9
 
-api.openweathermap.org / data / 2.5 / weather ? zip = [ZIP CODE], us & units=imperial & appid=[API KEY]
-api.openweathermap.org / data / 2.5 / weather ? zip = [01886], us & units=imperial & appid=[API KEY]
